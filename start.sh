7de28a2b0cab0cd7a10cbccf425077ef6f96140b
koiutyytr <128571154+koiutyytr@users.noreply.github.com>
2023-03-22 05:24:05 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#请修改为自己的UUID
+ead3c6ed-9148-4e9c-8159-1b47f986e702
+
+#请修改为自己设置的伪装站，不要带https://boredgamecompany.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
